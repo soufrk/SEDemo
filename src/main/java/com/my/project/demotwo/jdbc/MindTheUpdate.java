8ffc5ba96e6e0a86c1dc6d9ebc11fa12bf7c75ee
Souvik Goswami <soufrk@gmail.com>
2017-08-09 12:26:14 +0530

    Corrected the example

--- src/main/java/com/my/project/demotwo/jdbc/MindTheUpdate.java
+++ src/main/java/com/my/project/demotwo/jdbc/MindTheUpdate.java
@@ -10,40 +10,41 @@ import java.util.Formatter;
 
 /**
  * Predict why update fails.
+ * 
  * @author soufrk
  *
  */
 public class MindTheUpdate {
-
-    public static void main1(String[] args) throws SQLException {
-	String jdbcUrl = "jdbc:mariadb://localhost:3306/portal_mis";
+    
+    public static void main(String[] args) throws SQLException {
+	String jdbcUrl = "jdbc:mariadb://localhost:3306/test";
 	String uName = "root";
 	String pWord = "tiger";
 	Connection connection = null;
-	public static void main(String[] args) throws SQLException {
-		String jdbcUrl = "jdbc:mariadb://localhost:3306/portal_mis";
-		String uName = "root";
-		String pWord = "tiger";
-		Connection connection = null;
-		Formatter formatter = new Formatter();
-		String format = "%5s%40s\n";
-		try {
-			String update = "SELECT * FROM elapsed_time";
-			connection = DriverManager.getConnection(jdbcUrl, uName, pWord);
-			Statement statement = connection.createStatement(update);
-			ResultSet resultSet = statement.executeQuery();
-			if (resultSet != null) {
-				resultSet.next();
-				formatter.format(format, resultSet.getString(1), resultSet.getString(2));
-				resultSet.next();
-				formatter.format(format, resultSet.getString(1), resultSet.getString(2));
-			}
-			System.out.println(formatter);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			if (connection != null)
-			connection.close();
-		}
+	Formatter formatter = new Formatter();
+	String format = "%5s%40s\n";
+	try {
+	    connection = DriverManager.getConnection(jdbcUrl, uName, pWord);
+	    Statement statement1 = connection.createStatement();
+	    ResultSet resultSet1 = statement1.executeQuery("SELECT * FROM employee");
+	    if (resultSet1 != null) {
+		resultSet1.next();
+		formatter.format(format, resultSet1.getString(1), resultSet1.getString(2));
+		resultSet1.next();
+		formatter.format(format, resultSet1.getString(1), resultSet1.getString(2));
+	    }
+	    String update = "UPDATE employee SET salary = salary + 500";
+	    Statement statement2 = connection.createStatement(update);
+	    ResultSet resultSet2 = statement2.executeQuery();
+	    if(resultSet2 == null){
+		System.out.println("Updates !!");
+	    }
+	    System.out.println(formatter);
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} finally {
+	    if (connection != null)
+		connection.close();
 	}
+    }
 }
